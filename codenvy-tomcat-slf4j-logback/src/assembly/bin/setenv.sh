89d1d3f3c2fd936891ffc0f957f04e614cf8cff9
Sergii Kabashniuk <skabashnyuk@codenvy.com>
2015-05-18 18:13:31 +0300

    Set up logger

--- codenvy-tomcat-slf4j-logback/src/assembly/bin/setenv.sh
+++ codenvy-tomcat-slf4j-logback/src/assembly/bin/setenv.sh
@@ -1,1 +1,30 @@
-JAVA_OPTS="$JAVA_OPTS -Djuli-logback.configurationFile=file:$CATALINA_HOME/conf/logback.xml"
+#
+# CODENVY CONFIDENTIAL
+# __________________
+#
+#  [2012] - [2013] Codenvy, S.A.
+#  All Rights Reserved.
+#
+# NOTICE:  All information contained herein is, and remains
+# the property of Codenvy S.A. and its suppliers,
+# if any.  The intellectual and technical concepts contained
+# herein are proprietary to Codenvy S.A.
+# and its suppliers and may be covered by U.S. and Foreign Patents,
+# patents in process, and are protected by trade secret or copyright law.
+# Dissemination of this information or reproduction of this material
+# is strictly forbidden unless prior written permission is obtained
+# from Codenvy S.A..
+#
+
+if [ -z "${CODENVY_LOCAL_CONF_DIR}" ]; then
+    echo "Need to set CODENVY_LOCAL_CONF_DIR"
+    exit 1
+fi
+
+#Global JAVA options
+[ -z "${JAVA_OPTS}" ]  && JAVA_OPTS="-Xms256m -Xmx2048m -XX:MaxPermSize=256m -server"
+
+
+export CLASSPATH="$CLASSPATH:${CATALINA_HOME}/conf/:${JAVA_HOME}/lib/tools.jar"
+export JAVA_OPTS="$JAVA_OPTS -Djuli-logback.configurationFile=file:$CATALINA_HOME/conf/logback.xml -Dcodenvy.local.conf.dir=${CODENVY_LOCAL_CONF_DIR}"
+
